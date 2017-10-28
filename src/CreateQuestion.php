010117afb9b4eefdd29488d45b8ad7d3c5c8ec94
Adam Koza <adam.koza@mail.utoronto.ca>
2017-10-28 17:22:41 -0400

    Connected PreviewQuestion to database. See description
    Implemented dynamic file naming/storage. Added formula to saved txt
    file.

--- src/CreateQuestion.php
+++ src/CreateQuestion.php
@@ -8,9 +8,9 @@
 <body>
 
 <div class="jumbotron text-center">
-  <p>Question Creator</p> 
+  <p>Question Creator</p>
 </div>
-  
+
 <div class="container">
   <form method="post" action="PreviewQuestion.php">
     <div class="form-row">
@@ -25,12 +25,12 @@
 
         <div class="form-group">
           <div class="form-row">
-            <button type="button" class="btn btn-sm btn-primary">New Matrix</button> 
+            <button type="button" class="btn btn-sm btn-primary">New Matrix</button>
             <input type="text"> x <input type="text">
           </div>
         </div>
         <ul class="list-group" id="varList">
-          <li class="list-group-item"> 
+          <li class="list-group-item">
             x = 5
             <div class="form-group pull-right" role="group" aria-label="Basic example">
               <label class="form-check-label">
@@ -48,7 +48,7 @@
     <div class="form-row">
       <div class="col-sm-6">
         <h5>Question Formula</h5>
-        <input class="form-control" type="text" placeholder="Question Formula"</input>
+        <input id="formula" name="formula" class="form-control" type="text" placeholder="Question Formula"</input>
         <ol id="varList">
         </ol>
       </div>
@@ -56,6 +56,7 @@
       <div class="col-sm-6">
         <h5>Question Text</h5>
         <textarea id="questionText" name="questionText" class="form-control" rows="10" placeholder="Question Text" required></textarea>
+        <input type="hidden" name="assignment_id" id="assignment_id" value="<?php if (isset($_POST['assignment_id'])){ echo $_POST['assignment_id']; } else { echo 1; } ?>" />
       </div>
     </div>
 
--- src/PreviewQuestion.php
+++ src/PreviewQuestion.php
@@ -4,16 +4,45 @@
 </head>
 <body>
 <?php
-function saveString($questionInput){ // function that takes in a string and store into a file
-  $file_name = "question.txt";
-  file_put_contents($file_name, $questionInput);
+function saveString($filename, $questionInput){ // function that takes in a string and store into a file
+  file_put_contents($filename, $questionInput);
 }
 
 if (isset($_POST['questionText']))
 {
-	$question_text = $_POST['questionText'];
-	saveString($_POST["questionText"]); // saves the string in the textarea into the file
-	echo $question_text;
+
+        $dir = 'questions';
+
+        // create new directory with 744 permissions if it does not exist yet
+        // owner will be the user/group the PHP script is run under
+        if ( !file_exists($dir) ) {
+                $oldmask = umask(0);
+                mkdir ($dir, 0744);
+        }
+
+        // Increment question number for file name.
+        $fi = new FilesystemIterator($dir, FilesystemIterator::SKIP_DOTS);
+        $file_name = "/question" . (iterator_count($fi) + 1) . ".txt";
+
+        // Append answer to question.
+        $qanda = $_POST['questionText'] . "\n\n\n\n ANSWER: " . $_POST['formula'];
+
+        // Save question to file.
+        saveString($dir . $file_name, $qanda); // saves the string in the textarea into the file
+        echo $qanda;
+
+        // Determine how many rows exist in question table (for question_id).
+        $mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
+        $result = $mysqli->query("SELECT * FROM questions");
+        $question_id = $result->num_rows + 1;
+
+        // Insert question into question table
+        $location = $dir . $file_name;
+        $assignment_id = $_POST['assignment_id'];
+        $sql = "INSERT INTO questions (question_id, assignment_id, location) VALUES ($question_id, $assignment_id, '$location')";
+        $mysqli->query($sql);
+        $mysqli->close();
+
 }
 ?>
 </body>
