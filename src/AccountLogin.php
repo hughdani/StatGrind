ed08e72c5c41e7bfc18caafda09cb910bb16299c
Brii Quach <briiquach@gmail.com>
2017-11-08 01:50:05 -0800

    Implement account creation and log in functionality

--- src/AccountLogin.php
+++ src/AccountLogin.php
@@ -5,21 +5,90 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
+
+<?php 
+
+function check_creds($username, $password) {
+	/*
+	Return integer based on result of credential check:
+	Correct user/password: 0
+	Account doesn't exist: 1
+	Incorrect password: 2
+	*/
+        $mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
+	$sql = "SELECT username, password FROM users WHERE username = '$username'";
+	$result = $mysqli->query($sql);
+        $mysqli->close();
+	if ($result->num_rows == 0){
+		return 1;
+	} else if (strcmp($result->fetch_row()[1], $password) != 0) {
+		return 2;
+	} else {
+		return 0;
+	}
+}
+
+
+$logged_in = false;
+$err_msg = "";
+$username = "";
+
+if (isset($_POST["login_attempt"])) {
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	if (check_creds($username, $password) == 0){
+		$logged_in = true;
+       		$mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
+		$sql = "SELECT first_name, user_id FROM users WHERE username = '$username'";
+		$result = $mysqli->query($sql)->fetch_row();
+		$firstname = $result[0];
+		$userid = $result[1];
+        	$mysqli->close();
+	}
+	else if (check_creds($username, $password) == 1){
+		$err_msg = "Account does not exist";
+	}
+	else if (check_creds($username, $password) == 2){
+		$err_msg = "Incorrect password";
+	}
+}
+?>
 <body>
 
-<div class="jumbotron text-center">
+<div class="jumbotron text-center" <?php if ($logged_in == true) { echo "style = 'display:none'";} ?>>
   <p>Log In</p> 
 </div>
+<div class="jumbotron text-center" <?php if ($logged_in == false) { echo "style = 'display:none'";} ?>>
+  <p> <?php if (isset($firstname)){ echo "Welcome back, $firstname!"; }?></p> 
+</div>
+
   
-<div class="container">
-  <form method="post" action="Welcome.php">
-      Name: <input type="text" name="name"><br>
-      E-mail: <input type="text" name="email"><br>
-      <input type="submit" value ="Log In">
+<div class="container" <?php if ($logged_in == true) { echo "style = 'display:none'";} ?>>
+  <form method="post" action="AccountLogin.php">
+      Username: <input type="text" name="username" value = <?php echo "'$username'" ?> required><br>
+      Password: <input type="password" name="password" required><br>
+      <input type="submit" name="login_attempt" value ="Log In">
   </form>
+  <?php echo $err_msg ?>
 
   <form method="post" action="CreateAccount.php">
+      <input type="submit" value ="Create Account">
   </form>
+</div>
+
+<div class="container" <?php if ($logged_in == false) { echo "style = 'display:none'";} ?>>
+    <br><br>
+    <img src="http://2.bp.blogspot.com/-2RS14e7Z5Zs/VN0VyBmGKLI/AAAAAAAABfE/XcmQJWEp26o/s1600/racoon.jpg">
+    <br><br>
+    <form action="NewAssignmentPage.php" method="post">
+      <input type="hidden" name="user_id" id="user_id" <?php if (isset($userid)){ echo "value='$userid'"; }?>/>
+      <input type="submit" value ="Create a New Assignment">
+    </form>
+
+    <form action="CreateQuestion.php" method="post">
+      <input type="hidden" name="user_id" id="user_id" <?php if (isset($userid)){ echo "value='$userid'"; }?>"/>
+      <input type="submit" value ="Create a New Question">
+    </form>
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- src/CreateAccount.php
+++ src/CreateAccount.php
@@ -5,22 +5,73 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
-<body>
+<?php 
+
+function available($user_name) {
+        $mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
+	$sql = "SELECT username FROM users WHERE username = '$user_name'";
+	$num_rows = $mysqli->query($sql)->num_rows;
+        $mysqli->close();
+	return ($num_rows == 0);	
+}
+
+function register($username, $password, $first_name, $last_name, $account_type) {
+        $mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
+	$sql = "INSERT INTO users (username, password, first_name, last_name, account_type) VALUES ('$username', '$password', '$first_name', '$last_name', $account_type)";
+	$mysqli->query($sql);
+        $mysqli->close();
+	return;
+}
+
+$err_msg = ""; 
+
+if (isset($_POST["create_Account"])) {
+    $username = $_POST["user_name"];
+    $firstname = $_POST["first_name"];
+    $lastname = $_POST["last_name"];
+    $accounttype = $_POST["account_type"];
+    $pass1 = $_POST["password1"];
+    $pass2 = $_POST["password2"];
+    if (!available($username)){
+        $err_msg = "Username is unavailable"; 
+        $username = "";
+    }
+    else if ($pass1 != $pass2){
+        $err_msg = "Passwords do not match"; 
+    }
+    else {
+	register($username, $pass1, $firstname, $lastname, $accounttype);
+   	$username = "";
+    	$firstname = "";
+    	$lastname = "";
+        $err_msg = "Account Created";
+    }
+} 
+
+?>
+<body>	
 
 <div class="jumbotron text-center">
   <p>Create New Account</p> 
 </div>
-  
+
 <div class="container">
+  <form method="post" action="CreateAccount.php">
+      User Name: <input type="text" name="user_name" minlength="4" <?php if (isset($username)){ echo "value='$username'"; }?> required><br>
+      Password: <input type="password" name="password1" minlength="4" required><br>
+      Confirm Password: <input type="password" name="password2" minlength="4" required><br>
+      First Name: <input type="text" name="first_name" <?php if (isset($firstname)){ echo "value='$firstname'"; }?> required><br>
+      Last Name: <input type="text" name="last_name" <?php if (isset($lastname)){ echo "value='$lastname'"; }?> required><br>
+      Account Type: <select name="account_type" required>
+ 			<option value="2">Student</option>
+  			<option value="1">Instructor</option>
+  			<option value="3">TA</option>
+		    </select><br>
+      <input type="submit" name="create_Account" value ="Create Account">
+      <?php echo "<br> $err_msg"; ?>
+  </form>
   <form method="post" action="AccountLogin.php">
-      User Name: <input type="text" name="user_name"><br>
-      Password: <input type="text" name="password1"><br>
-      Confirm Password: <input type="text" name="password2"><br>
-      First Name: <input type="text" name="first_name"><br>
-      Last Name: <input type="text" name="last_name"><br>
-      Account Type: <input type="text" name="acc_type"><br>
-      <input type="submit" value ="Create Account">
-    </div>
+      <input type="submit" name="log_in" value ="Log in">
   </form>
 </div>
 
