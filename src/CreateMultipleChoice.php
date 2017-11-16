5a2767e0ef0dec39a8d4aecefe745aa57bed4d5b
hughdani <daniel.hugh@mail.utoronto.ca>
2017-11-16 03:06:01 -0500

    Change MC question submission to utilize existing PreviewQuestion.php

--- src/CreateMultipleChoice.php
+++ src/CreateMultipleChoice.php
@@ -18,11 +18,11 @@
     <form action="CreateMultipleChoice.php" method="post">
         <div class="form-group">
             <label for="mc_question"> Question:</label>
-            <textarea type="submit" class="form-control" rows="5" name="mc_question" id="mc_question" required value=<?php echo $mc_question; ?>></textarea>
+            <textarea type="submit" class="form-control" rows="5" name="mc_question" id="mc_question" required value="<?php echo $mc_question; ?>"></textarea>
         </div>  
         <div class="form-group">
             <label for="num_question"> Number of Options:</label>
-            <input class="form-control" type="number" id="num_options" name="num_options" min="2" required value=<?php echo $num_options; ?>>
+            <input class="form-control" type="number" id="num_options" name="num_options" min="2" required value="<?php echo $num_options; ?>">
             <input type="submit" class="btn btn-default" name="new_mc" id="new_mc" value="Create Multiple Choice Question"> 
         </div>   
     </form>
@@ -30,22 +30,29 @@
     <?php 
         if(isset($_POST["new_mc"])) {
             $num_options = $_POST["num_options"];
+            echo "<form action='PreviewQuestion.php' method='post'>";
+            echo "<div class='form-group'>";
             echo "<h3>Options</h3>";
             for($i = 1; $i <= $num_options; $i++) {
-                echo "Option " . $i . " <input class='form-control' type='text' id=mc".$i . " value=''> " . "<br>";
+                echo "Option " . $i . " <input class='form-control' type='text' id=mc".$i . " value='' required> " . "<br>";
             }
             echo "Correct Option ID: " . 
-            "<input class='form-control' type='number' id='correct_opt' name='correct_opt' min='2' required value=" . $correct_opt . "></div>";
+            "<input class='form-control' type='number' id='correct_opt' name='correct_opt' min='1' max='" .
+            $num_options . "' required value='" . $correct_opt . "'></div>";
 
-        }
-        
-        echo "<h3>Summary</h3>";
-        echo "Question: " . $_POST["mc_question"] . "<br>";
-        echo "Options: " . $_POST["num_options"];
+                   
+            echo "<h3>Summary</h3>";
+            echo "Question: " . $_POST["mc_question"] . "<br>";
+            echo "Options: " . $_POST["num_options"];
 
-        
+            // questionFormula set to dummy filler value for it to work with existing PreviewQuestion.php
+            echo"<input class='hidden' name=questionText id=questionText value='". $_POST['mc_question'] . "'>
+                <input class='hidden' name=questionFormula id=questionFormula value='MCdummyvalue' >
+                <input type='submit' class='btn' value='Submit'> 
+                </div>
+                </form>";
+            }
     ?>
-
 </div>
 
-<!-- submit with id mc -->
+<!-- need to submit with the correct option, change previewquestion.php to handle this case -->
