b7f7245f22ddb293229105446c38577ebfc0e655
hughdani <daniel.hugh@mail.utoronto.ca>
2017-11-21 00:33:09 -0500

    Make basic outline for navbar

---
+++ src/NavigationBar.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>Create Multiple Choice</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="AccountLogin.php"><span class="glyphicon glyphicon-home"></span> StatGrind</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="NewAssignmentPage.php"><span class="glyphicon glyphicon-plus"></span> New Assignment</a></li>
+      <li><a href="CreateQuestion.php"><span class="glyphicon glyphicon-plus"></span> New Question</a></li>
+      <li><a href="AssignmentMarkingFeedback.php"><span class="glyphicon glyphicon-check"></span> Marking & Feedback</a></li>
+      <li><a href="EditCourses.php"><span class="glyphicon glyphicon-cog"></span> Manage Course</a></li>
+      <!-- 
+      <li><a href="AssignmentOverview.php">Assignment Overview</a></li>
+      <li><a href="ChooseAssignment.php">Write Assignment</a></li>
+      <li><a href="DisplayAssignment.php">Assignment Contents</a></li>
+      <li><a href="AssignmentAnswer.php">Assignment Answers</a></li>
+      <li><a href="ViewStatistic.php">Statistics</a></li>
+      -->
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="AccountLogin.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="AccountLogin.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+</body>
+</html>
