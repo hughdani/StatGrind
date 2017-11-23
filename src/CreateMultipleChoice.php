e37d7c5f3f16a8a896335ad1cb13469e2fbe96bb
hughdani <daniel.hugh@mail.utoronto.ca>
2017-11-23 01:15:37 -0500

    Change variables to camelCase and use <?= ?> instead of echos

--- src/CreateMultipleChoice.php
+++ src/CreateMultipleChoice.php
@@ -17,41 +17,41 @@
 
     <form action="CreateMultipleChoice.php" method="post">
         <div class="form-group">
-            <label for="mc_question"> Question:</label>
-            <textarea type="submit" class="form-control" rows="5" name="mc_question" id="mc_question" required value="<?php echo $mc_question; ?>"></textarea>
+            <label for="mcQuestion"> Question:</label>
+            <textarea type="submit" class="form-control" rows="5" name="mcQuestion" id="mcQuestion" required value="<?=$mcQuestion;?>"></textarea>
         </div>  
         <div class="form-group">
-            <label for="num_question"> Number of Options:</label>
-            <input class="form-control" type="number" id="num_options" name="num_options" min="2" required value="<?php echo $num_options; ?>">
-            <input type="submit" class="btn btn-default" name="new_mc" id="new_mc" value="Create Multiple Choice Question"> 
+            <label for="numOptions"> Number of Options:</label>
+            <input class="form-control" type="number" id="numOptions" name="numOptions" min="2" required value="<?=$numOptions;?>">
+            <input type="submit" class="btn btn-default" name="newMC" id="newMC" value="Create Multiple Choice Question"> 
         </div>   
     </form>
 
-    <?php if(isset($_POST["new_mc"])) : ?>
-        <?php $num_options = $_POST["num_options"]; ?>
+    <?php if(isset($_POST["newMC"])) : ?>
+        <?php $numOptions = $_POST["numOptions"]; ?>
+
         <form action='PreviewQuestion.php' method='post'>
             <div class='form-group'>
                 <h3>Options</h3>
 
-                <?php for($i = 1; $i <= $num_options; $i++) : ?>
-                    <p>Option <?= $i ?> </p>
+                <?php for($i = 1; $i <= $numOptions; $i++) : ?>
+                    <p>Option <?=$i?> </p>
                     <input class='form-control' type='text' id=mc<?=$i?> value='' required> 
                     <br>
                 <?php endfor; ?>
 
                 <p>Correct Option ID:</p>
                 <input class='form-control' type='number' id='correct_opt' name='correct_opt' min='1' 
-                max=<?=$num_options?> required value=<?=$correct_opt?>>
+                max=<?=$numOptions?> required value=<?=$correct_opt?>>
             </div>
-
-                    
+                   
             <h3>Summary</h3>
-            <div>Question: <?=$_POST["mc_question"]?> </div>
+            <div>Question: <?=$_POST["mcQuestion"]?> </div>
             <br>
-            <div>Options: <?=$_POST["num_options"]?> </div>
+            <div>Options: <?=$_POST["numOptions"]?> </div>
 
             <!-- questionFormula set to dummy filler value for it to work with existing PreviewQuestion.php -->
-            <input class='hidden' name=questionText id=questionText value=<?=$_POST['mc_question']?>>
+            <input class='hidden' name=questionText id=questionText value=<?=$_POST['mcQuestion']?>>
             <input class='hidden' name=questionFormula id=questionFormula value='2+2' >
             <input type='submit' class='btn' value='Submit'> 
         </form>
