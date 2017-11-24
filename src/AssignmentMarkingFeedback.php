c1ce89d427f1bef184138d5886b10a6690c99a51
Kevin Choi <kev.choi@mail.utoronto.ca>
2017-11-24 12:08:45 -0500

    Add dropdown for selecting assignment and break code to function

--- src/AssignmentMarkingFeedback.php
+++ src/AssignmentMarkingFeedback.php
@@ -13,6 +13,72 @@
 		<h1>Assignment Marking/Feedback</h1>
 	</div>
 <?php
+/**
+ * Update the mark and feedback for the selected student's assignment
+ *
+ * @param int $new_mark the updated mark for the selected assignment
+ * @param int $new_feed_back the updated feedback for the selected assignment
+ * @param int $attempt_id the current attempt id for the selected assignment
+ * @param int $student_id the current selected student
+ */
+function update_mark_and_feedback($new_mark, $new_feed_back, $attempt_id, $student_id){
+	if ($attempt_id != 0){
+		$mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
+		$sqlUpdate = "UPDATE results SET result=$newMark, feedback='$newFeedback' WHERE attempt_id=$attempt_id and student_id=$student_id";
+		$mysqli->query($sqlUpdate);
+		$mysqli->close();
+	}
+}
+
+/**
+ * Display the dropdown for selecting assignment and display the mark/feedback of the selected assignment
+ *
+ * @param date $current_time the current time of the search
+ * @param int $student_id the current selected student
+ */
+function display_mark_and_feedback($current_time, $student_id){
+?>
+	<form method='post'>
+		View Mark/Feedback for: 
+		<select name="selectedAssignment" onchange="this.form.submit();">
+			<option disabled value="" selected hidden>Select Assignment</option>
+			<?php 
+			// Display open assignments.
+			$mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
+			$sql = "SELECT assignment_id, start_date FROM assignments";
+			$result = $mysqli->query($sql);
+			while ($row = $result->fetch_row()){
+				if ($current_time > $row["start_date"]) {
+					echo "<option value='".$row['assignment_id']."''> Assignment". $row['assignment_id'] . "</option>";
+				}
+			}
+			?>
+		</select>
+		<br>
+<?php
+	if(isset($_POST['selectedAssignment'])){
+		echo "<h2>Assignment $row["selectedAssignment"]</h2><br>";
+		// Select all student attempts for this assignment.
+		$sql = "SELECT result, attempt_id, feedback FROM results WHERE attempt_id = (SELECT MAX(attempt_id) FROM results WHERE student_id = '$student_id' AND assignment_id = $row["selectedAssignment"])";
+		$result2 = $mysqli->query($sql);
+		$row2 = $result2->fetch_row()
+		$feedback = $row2["feedback"];
+		$attempt_id = $row2["attempt_id"];
+		$mark = $row2["result"];
+		echo "Mark: " . $mark . "<br>Number of attempts: " . $attempt_id . "<br>";
+		echo "Instructor feedback: " . $feedback . "<br>";
+	}
+	$mysqli->close();
+?>	
+    	<input type="hidden" name="student_id" id="student_id" value="<?php echo $student_id; ?>"/>
+    	<input type="hidden" name="attempt_id" id="attempt_id" value="<?php echo $attempt_id; ?>"/>
+		<input id="newmark" name="newmark" type='text' class='form-control' text="<?php echo $mark; ?>">
+  		<textarea id="feedback" name="feedback" class='form-control' rows='5' text="<?php echo $feedback; ?>"></textarea>
+  		<input type="submit" class="btn btn-default" value="Submit Update"/>
+    </form>
+<?php
+}
+
 	// Get student_id
 	if (isset($_POST['student_id'])) {
 		$student_id = $_POST['student_id'];
@@ -21,25 +87,7 @@
 	}
 
 	if(isset($_POST['attempt_id'])){
-		$newMark = $_POST['newmark'];
-		$newFeedback = $_POST['feedback'];
-		$attempt_id = $_POST['attempt_id'];
-		if ($attempt_id != 0){
-			$mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
-			If (($newMark == "") and ($newFeedback != "")){
-				$sqlUpdate = "UPDATE results SET feedback='$newFeedback' WHERE attempt_id=$attempt_id";
-				$mysqli->query($sqlUpdate);
-			}
-			if (($newMark != "") and ($newFeedback == "")){
-				$sqlUpdate = "UPDATE results SET result=$newMark WHERE attempt_id=$attempt_id";
-				$mysqli->query($sqlUpdate);
-			}
-			if (($newMark != "") and ($newFeedback != "")){
-				$sqlUpdate = "UPDATE results SET result=$newMark, feedback='$newFeedback' WHERE attempt_id=$attempt_id";
-				$mysqli->query($sqlUpdate);
-			}
-			$mysqli->close();
-		}
+		update_mark_and_feedback($_POST['newmark'], $_POST['feedback'],$_POST['attempt_id'], $_POST['student_id']);
 	}
 
 	// Get current time, convert to 24hr.
@@ -53,55 +101,8 @@
 		$current_time = $edit_time[0] . " " . $edit_hour[0] . ":" . $edit_hour[1] . ":" . $edit_hour[2][0] .  $edit_hour[2][1];
 	}
 	// Select all assignment from assignment table.
-	$mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
-	$sql = "SELECT assignment_id, start_date FROM assignments";
-	$result = $mysqli->query($sql);
-	// Display open assignments.
-	while ($row = $result->fetch_row()) {
-		if ($current_time > $row[1]) {
-			echo "<h2>Assignment $row[0]</h2><br>";
-			// Select all student attempts for this assignment.
-			$sql = "SELECT result, attempt_id, feedback FROM results WHERE student_id = '$student_id' AND assignment_id = $row[0]";
-			$result2 = $mysqli->query($sql);
-			$attempts = $result2->num_rows;
-			$feedback = "";
-			$attempt_id = 0;
-			// Determine last attempt.
-			if ($attempts > 0) {
-				$mark = 0;
-				$attempt_id = 0;
-				while ($row2 = $result2->fetch_row()) {
-					if ($row2[1] > $attempt_id) {
-						$mark = $row2[0];
-						$attempt_id = $row2[1];
-						$feedback = $row2[2];
-					}
-				}
-			} else {
-				$mark = 0;
-			}
-			echo "Mark: " . $mark . "<br>Number of attempts: " . $attempts . "<br>";
-			echo "Instructor feedback: " . $feedback . "<br>";
-
+	display_mark_and_feedback($current_time, $student_id);
 ?>
-
-        <form method='post'>
-        	<input type="hidden" name="student_id" id="student_id" value="<?php echo $student_id; ?>"/>
-        	<input type="hidden" name="attempt_id" id="attempt_id" value="<?php echo $attempt_id; ?>"/>
-		<input id="newmark" name="newmark" type='text' class='form-control' placeholder="<?php echo $mark; ?>">
-      		<textarea id="feedback" name="feedback" class='form-control' rows='5' placeholder="<?php echo $feedback; ?>"></textarea>
-      		<input type="submit" class="btn btn-default" value="Submit Update"/>
-        </form>
-
-        <?php
-
-	}
-	
-}
-$mysqli->close();
-?>
-
-
 
 </div>
 </body>
