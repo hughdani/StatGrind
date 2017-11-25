1fc5bb8e90d6516f2d3daaaed8f2f722d45dac0c
Brii Quach <briiquach@gmail.com>
2017-11-25 11:16:39 -0800

    Add option to update feedback for every student

--- src/AssignmentMarkingFeedback.php
+++ src/AssignmentMarkingFeedback.php
@@ -13,30 +13,32 @@
 		<h1>Assignment Marking/Feedback</h1>
 	</div>
 <?php
+
+
 /**
  * Update the mark and feedback for the selected student's assignment
  *
  * @param int $new_mark the updated mark for the selected assignment
  * @param int $new_feed_back the updated feedback for the selected assignment
  * @param int $attempt_id the current attempt id for the selected assignment
- * @param int $student_id the current selected student
  */
-function update_mark_and_feedback($new_mark, $new_feed_back, $attempt_id, $student_id){
-	if ($attempt_id != 0){
-		$mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
-		$sqlUpdate = "UPDATE results SET result=$newMark, feedback='$newFeedback' WHERE attempt_id=$attempt_id and student_id=$student_id";
-		$mysqli->query($sqlUpdate);
-		$mysqli->close();
-	}
+function update_mark_and_feedback($new_mark, $new_feedback, $attempt_id){
+	$mysqli = new mysqli("localhost", "root", "R0binson", "CSCC01");
+	$sqlUpdate = "UPDATE results SET result=$new_mark, feedback='$new_feedback' WHERE attempt_id=$attempt_id";
+	$mysqli->query($sqlUpdate);
+	$mysqli->close();
 }
+
+if(isset($_POST['attempt_id'])){
+	update_mark_and_feedback($_POST['newmark'], $_POST['feedback'],$_POST['attempt_id']);
+}
+
 
 /**
  * Display the dropdown for selecting assignment and display the mark/feedback of the selected assignment
  *
- * @param date $current_time the current time of the search
- * @param int $student_id the current selected student
  */
-function display_mark_and_feedback($current_time, $student_id){
+function display_mark_and_feedback(){
 ?>
 	<form method='post'>
 		View Mark/Feedback for: 
@@ -48,61 +50,45 @@ function display_mark_and_feedback($current_time, $student_id){
 			$sql = "SELECT assignment_id, start_date FROM assignments";
 			$result = $mysqli->query($sql);
 			while ($row = $result->fetch_row()){
-				if ($current_time > $row[1]) {
-					echo "<option value='".$row[0]."''> Assignment ". $row[0] . "</option>";
-				}
+				echo "<option value='".$row[0]."''> Assignment ". $row[0] . "</option>";
 			}
 			?>
 		</select>
+	</form>
 		<br>
 <?php
-	if(isset($_POST['selectedAssignment'])){
-		$assignment_id = $_POST['selectedAssignment'];
+	if(isset($_POST['select_assignment'])){
+		$assignment_id = $_POST['select_assignment'];
 		echo "<h2>Assignment $assignment_id</h2><br>";
 		// Select all student attempts for this assignment.
-		$sql = "SELECT result, attempt_id, feedback FROM results WHERE attempt_id = (SELECT MAX(attempt_id) FROM results WHERE student_id = '$student_id' AND assignment_id = $assignment_id)";
-		$result2 = $mysqli->query($sql);
-		$row2 = $result2->fetch_row();
-		$feedback = $row2[2];
-		$attempt_id = $row2[1];
-		$mark = $row2[0];
-		echo "Mark: " . $mark . "<br>Number of attempts: " . $attempt_id . "<br>";
-		echo "Instructor feedback: " . $feedback . "<br>";	
-?>	
-    	<input type="hidden" name="student_id" id="student_id" value="<?php echo $student_id; ?>"/>
-    	<input type="hidden" name="attempt_id" id="attempt_id" value="<?php echo $attempt_id; ?>"/>
-		<input id="newmark" name="newmark" type='text' class='form-control' value="<?php echo $mark; ?>">
-  		<textarea id="feedback" name="feedback" class='form-control' rows='5'><?php echo $feedback; ?></textarea>
-  		<input type="submit" class="btn btn-default" value="Submit Update"/>
-    </form>
+		$sql = "SELECT results.student_id, results.assignment_id, results.result, results.feedback, results.attempt_id from (SELECT student_id, assignment_id, max(attempt_id) as 'most_recent' FROM `results` group by student_id, assignment_id) T LEFT JOIN results on most_recent = attempt_id where results.assignment_id = $assignment_id";
+		$result = $mysqli->query($sql);
+		while($row = $result->fetch_assoc()){ ?>
+	<form id="update_result" method='post'>
+			<?php
+			$student_id = $row['student_id'];
+			$feedback = $row['feedback'];
+			$mark = $row['result'];
+			$attempt_id = $row['attempt_id'];
+			echo "Student ID: $student_id <br>";
+			?>	
+    			<input type="hidden" name="student_id" id="student_id" value="<?php echo $student_id; ?>"/>
+    			<input type="hidden" name="attempt_id" id="attempt_id" value="<?php echo $attempt_id; ?>"/>
+    			<input type="hidden" name="select_assignment" id="select_assignment" value="<?php echo $assignment_id; ?>"/>
+			Mark:
+			<input id="newmark" name="newmark" type='text' class='form-control' value="<?php echo $mark; ?>">
+			Feedback:
+  			<textarea id="feedback" name="feedback" type='text' class='form-control' form='update_result' rows='5' ><?php echo $feedback; ?></textarea>
+  			<input type="submit" class="btn btn-default" value="Submit Update"/>
+    	</form>
 <?php
+		}
 	}
 	$mysqli->close();
 }
 
-	// Get student_id
-	if (isset($_POST['student_id'])) {
-		$student_id = $_POST['student_id'];
-	} else {
-		$student_id = "kozaadam";
-	}
+display_mark_and_feedback();
 
-	if(isset($_POST['attempt_id'])){
-		update_mark_and_feedback($_POST['newmark'], $_POST['feedback'],$_POST['attempt_id'], $_POST['student_id']);
-	}
-
-	// Get current time, convert to 24hr.
-	$current_time = date("Y-m-d h:i:sa");
-	$edit_time = explode(" ", $current_time);
-	$edit_hour = explode(":", $edit_time[1]);
-	if ($edit_hour[2][2] == "p") {
-		$new_hour = $edit_hour[0] + 12;
-		$current_time = $edit_time[0] . " " . $new_hour . ":" . $edit_hour[1] . ":" . $edit_hour[2][0] .  $edit_hour[2][1];
-	} else {
-		$current_time = $edit_time[0] . " " . $edit_hour[0] . ":" . $edit_hour[1] . ":" . $edit_hour[2][0] .  $edit_hour[2][1];
-	}
-	// Select all assignment from assignment table.
-	display_mark_and_feedback($current_time, $student_id);
 ?>
 
 </div>
