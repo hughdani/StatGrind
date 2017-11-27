8e1599165ac527580b1be3169322fd342a785d67
Darius Asri Rezaei <darius.asri@gmail.com>
2017-11-27 01:04:42 +0000

    Code overhaul

--- src/CreateCourse.php
+++ src/CreateCourse.php
@@ -1,12 +1,20 @@
-<html>
-<head>
-    <title>Edit My Courses</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/main.css" />
-</head>
-<h1>Add a New Course</h1>
+<?php
+require_once 'Database.php';
+require_once 'User.php';
+require_once 'Utils.php';
+$db = new Database();
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+if (!isset($_SESSION['user'])) {
+    header("Location: Forbidden.php");
+} elseif (!$db->pagePermission(basename(__FILE__), $_SESSION['user'])) {
+    header("Location: Forbidden.php");
+}
+
+create_head('Create Course');
+?>
 <form method='post' action='EditCourses.php'>
   <div class='form-group'>
     <label for='new_cname'>New Course Name</label>
@@ -21,31 +29,3 @@
   <div class='form-group'>
     <input type='submit' name='create_course' value='Add Course' class='btn btn-default'> 
 </form>
-<?php
-session_start();
-$mysqli = new mysqli('localhost', 'root', 'R0binson', 'CSCC01');
-
-$user_id = $_SESSION['user_id'];
-$user_id = 124;
-
-if (!permission_check($mysqli, $user_id)) {
-    header('Location: Forbidden.php');
-}
-$mysqli->close();
-
-/**
- * Returns true if the user_id belongs to an instructor
- *
- * @param int $user_id  the id of the user accessing the page
- */
-function permission_check($mysqli, $user_id) {
-    $query = "";
-    $query = $query . "SELECT * FROM users LEFT JOIN account_types ";
-    $query = $query . "ON users.account_type = account_types.account_type ";
-    $query = $query . "WHERE users.user_id = $user_id AND account_types.type_description='Instructor'";
-    // search the current user in instructor records
-    $result = $mysqli->query($query);
-    return ($result->num_rows != 0);
-}
-?>
-
