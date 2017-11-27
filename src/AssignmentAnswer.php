ae033be4bcf557d23d7567a3721024f53f612b5d
Brian Quach <quachbr2@IITS-I406-02.utsc-labs.utoronto.ca>
2017-11-27 15:38:51 -0500

    Add header to assignmentanswer page

--- src/AssignmentAnswer.php
+++ src/AssignmentAnswer.php
@@ -1,13 +1,32 @@
 <?php
+require_once 'Database.php';
+require_once 'User.php';
 require_once 'Utils.php';
 
 if (!isset($_SESSION)) {
     session_start();
 }
+if (!isset($_SESSION['user'])) {
+    header("Location: Forbidden.php");
+}
 
-create_head('Assignment Answer');
+create_head('Home');
+echo "<body>";
+
+$db = new Database();
+$user = $_SESSION['user'];
+$first_name = $user->getFirstName();
+$account_type = $user->getAccountType();
+$header_text = "Welcome back $first_name!";
+
+
+include("NavigationBar.php");
+create_site_header($header_text);
 ?>
-<body>
+
+<div class="container-fluid">
+<section class="wrapper style2 special">
+<div class="inner narrow">
 <form method="POST">
     Assignment ID: <input type="text" name="assignment_id"><br>
     <input type="submit" name="submit" value="Search Answer">
@@ -21,5 +40,8 @@ if(isset($_POST['submit'])) {
     GetAssignment();
 }
     ?>
+</div>
+</section>
+</div>
 </body>
 </html>
