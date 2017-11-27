7a5aaf5bb0f7aeebbb49e01778da49b52181c014
Darius Asri Rezaei <darius.asri@gmail.com>
2017-11-27 22:43:10 +0000

    Fix code order messed up by git

--- src/SelectQuestionType.php
+++ src/SelectQuestionType.php
@@ -2,18 +2,16 @@
 require_once 'Database.php';
 require_once 'User.php';
 require_once 'Utils.php';
+if (!isset($_SESSION)) {
+    session_start();
+}
+
 if (!isset($_SESSION['user'])) {
 header("Location: error.php?error_status=401");
 exit();
 } elseif (!$db->pagePermission(basename(__FILE__), $_SESSION['user'])) {
 header("Location: error.php?error_status=403");
 exit();
-}
-if (!isset($_SESSION)) {
-    session_start();
-}
-if (!isset($_SESSION['user'])) {
-    header("Location: Forbidden.php");
 }
 
 create_head('Select Question Type');
