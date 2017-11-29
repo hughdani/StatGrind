3c6bc2902c2063b26ea749a3c924d4de052e5f4b
Kevin Choi <kev.choi@mail.utoronto.ca>
2017-11-28 23:26:19 -0500

    Fix header to error page

--- src/ViewStatistic.php
+++ src/ViewStatistic.php
@@ -2,19 +2,23 @@
 require_once 'Database.php';
 require_once 'User.php';
 require_once 'Utils.php';
+$db = new Database();
+$mysqli = $db->getconn();
 
 if (!isset($_SESSION)) {
     session_start();
 }
 if (!isset($_SESSION['user'])) {
-    header("Location: Forbidden.php");
+    header("Location: error.php?error_status=401");
+    exit();
+} elseif (!$db->pagePermission(basename(__FILE__), $_SESSION['user'])) {
+    header("Location: error.php?error_status=403");
+    exit();
 }
 
 create_head('Assignment Statistics');
 echo "<body>";
 
-$db = new Database();
-$mysqli = $db->getconn();
 $user = $_SESSION['user'];
 $first_name = $user->getFirstName();
 $account_type = $user->getAccountType();
