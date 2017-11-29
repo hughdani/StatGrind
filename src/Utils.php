6cbdc64c7876125332260692d0a879cda5308b98
Brii Quach <briiquach@gmail.com>
2017-11-28 21:45:32 -0800

    Add site icon

--- src/Utils.php
+++ src/Utils.php
@@ -10,6 +10,7 @@ function create_head($title)
     echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>";
     echo "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>";
     echo "<link rel='stylesheet' href='css/main.css' />";
+    echo "<link rel='icon' href='css/favicon.png'>";
     echo "</head>";
 }
 
