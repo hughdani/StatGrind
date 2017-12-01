804d915e4ea0a019c0a2929f0ea47cee22f7e79f
Darius Asri Rezaei <darius.asri@gmail.com>
2017-12-01 07:29:21 +0000

    Choose assignments course, visibility filtered - tags also work

--- src/ChooseAssignment.php
+++ src/ChooseAssignment.php
@@ -20,6 +20,7 @@ echo "<body>";
 
 $user = $_SESSION['user'];
 $first_name = $user->getFirstName();
+$user_id = $user->getUserId();
 $account_type = $user->getAccountType();
 $header_text = "Write Assignment";
 
@@ -37,11 +38,11 @@ Find Assignment:
 </form>
 <?php
 // Select all assignments where the end date hasn't passed
-$sql = "SELECT assignment_id, end_date FROM assignments WHERE end_date > NOW() AND start_date <= NOW() AND visible=true";
+$sql = "SELECT assignment_id, end_date FROM assignments INNER JOIN taking_course ON assignments.course_id = taking_course.course_id WHERE end_date > NOW() AND start_date <= NOW() AND visible=true AND taking_course.user_id = $user_id";
 
 //Apply search params if any
 if (isset($_POST['search_param'])) {
-	$sql = $sql . " AND tag LIKE '%".$_POST['search_param']."%' OR title LIKE '%".$_POST['search_param']."%'";
+	$sql = $sql . " AND (tag LIKE '%".$_POST['search_param']."%' OR title LIKE '%".$_POST['search_param']."%')";
 }
 $assignments = $mysqli->query($sql);
 // Loop through and display each assignment
@@ -51,19 +52,19 @@ while ($row = $assignments->fetch_assoc()) {
 	$end_date = $row["end_date"];
 	echo "<h2>$assignment_title </h2><br>";
 	// Get previous attempt mark
-	$sql = "SELECT result FROM results WHERE student_id = '$student_id' AND assignment_id = $assignment_id ORDER BY attempt_id DESC";
+	$sql = "SELECT result FROM results WHERE student_id = '$user_id' AND assignment_id = $assignment_id ORDER BY attempt_id DESC";
 	$result = $mysqli->query($sql);
 	$attempts = $result->num_rows;
-	if ($attempts > 0){
-		$mark = $result->fetch_assoc()["result"];
+	if ($attempts > 0) {
+		$mark = $result->fetch_assoc()["result"] . "%";
 	} else {
 		$mark = "N/A";
 	}
 	echo "Available until : " . $end_date . "<br>";
-	echo "Last Attempt: " . $mark . "%<br>";
+	echo "Last Attempt: " . $mark . "<br>";
 	echo "<form action='WriteAssignment.php' method='post'>";
 	echo "<input type='hidden' name='assignment_id' id='assignment_id' value='$assignment_id'>";
-	echo "<input type='hidden' name='student_id' id='student_id' value='$student_id'>";
+	echo "<input type='hidden' name='student_id' id='student_id' value='$user_id'>";
 	echo "<input type='submit' value ='Write This Assignment'>";
         echo "</form><br>";
 	
